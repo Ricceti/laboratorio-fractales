b1018aac1151e76e75f12aa1e0d64c4655e28eaa
ricceti <ddabensur@gmail.com>
2026-06-02 13:30:07 -0500

    Modificacion de Curva Levy para que cambie el color según el nivel de recursión

--- examples/curvaLevy.c
+++ examples/curvaLevy.c
@@ -1,17 +1,23 @@
 #include <math.h>
 #include "../turtlec.h"
 
-void levy(Turtle *t, float length, int depth){
+void levy(Turtle *t, float length, int depth, int colorIndex){
   if(depth == 0){
+    int c = colorIndex % 5;
+    if(c == 0)       turtleSetColor(t, 255, 0, 0);
+    else if(c == 1)  turtleSetColor(t, 255, 165, 0);
+    else if(c == 2)  turtleSetColor(t, 255, 255, 0);
+    else if(c == 3)  turtleSetColor(t, 0, 200, 255);
+    else             turtleSetColor(t, 180, 0, 255);
     turtleForward(t, length);
     return;
   }
 
   turtleLeft(t, 45);
-  levy(t, length / sqrt(2), depth - 1);
+  levy(t, length / sqrt(2), depth - 1, colorIndex + 1);
 
   turtleRight(t, 90);
-  levy(t, length / sqrt(2), depth - 1);
+  levy(t, length / sqrt(2), depth - 1, colorIndex + 2);
 
   turtleLeft(t, 45);
 }
@@ -25,7 +31,7 @@ int main(void){
   turtleGoTo(t, 150.0f, 300.0f);
   turtlePenDown(t);
   turtleSetSpeed(t, 40.0f);
-  levy(t, 300, 10);
+  levy(t, 300, 10, 10);
   turtleAppRun(app);
   turtleAppDestroy(app);
   return 0;
